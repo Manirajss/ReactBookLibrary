36276f423fbc6f733c07bbc3ae2555a6878cb762
Manirajss <Manirajss@users.noreply.github.com>
2018-04-09 17:51:01 +0530

    Initial Commit
    Signed-off-by: Manirajss <Manirajss@users.noreply.github.com>

---
+++ src/Book/books.container.js
@@ -0,0 +1,265 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import 'antd/dist/antd.css';
+// import './index.css';
+ import {Button,notification } from "antd"
+import BookComponent from "./books.component";
+
+// const Search = Input.Search;
+// const InputGroup = Input.Group;
+// const Option = Select.Option;
+const defaultDataSource=[{
+  key: '1',
+  bookName: "Man's search for meaning",
+  isbn: 9788971460108,
+  author:"Viktor Frankl",
+  price: 5,
+  availableItems: 7,
+  category: "Philosophy"
+}, {
+  key: '2',
+  bookName: 'Wings of Fire: An Autobiography 1st Edition',
+  author:"Dr.A. P. J. Abdul Kalam",
+  isbn: 9788173154195,
+  price: 4,
+  availableItems: 8,
+  category: "Autobiography"
+}, {
+  key: '3',
+  bookName: 'Playing It My Way',
+  author:"Sachin Tendulkar",
+  isbn: 9781473612549,
+  price: 4,
+  availableItems: 6,
+  category: "Autobiography"
+}, {
+  key: '4',
+  bookName: 'From Here to Eternity',
+  author:"JAMES JONES",
+  isbn: 9780385333641,
+  price: 4,
+  availableItems: 2,
+  category: "Historical Fiction"
+}, {
+  key: '5',
+  bookName: 'The Greatest Show on Earth: The Evidence for Evolution',
+  author:"Richard Dawkins",
+  isbn: 9780593061732,
+  price: 2,
+  availableItems: 1,
+  category: "Science"
+}];
+
+class BookContainer extends Component {
+    constructor(props) {
+      super(props);
+      var requestColumns = [{
+        title: 'Book Name',
+        dataIndex: 'bookName',
+        key: 'bookName',
+      }, {
+        title: 'ISBN',
+        dataIndex: 'isbn',
+        key: 'isbn',
+      },{
+        title: 'Author',
+        dataIndex: 'author',
+        key: 'author',
+      },{
+        title: 'Price($)',
+        dataIndex: 'price',
+        key: 'isbn',
+      },{
+        title: 'Available items',
+        dataIndex: 'availableItems',
+        key: 'isbn',
+      },{
+        title: 'Category',
+        dataIndex: 'category',
+        key: 'isbn',
+      },{
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <Button 
+            type="primary"
+            onClick={this.requestConfirm.bind(this)}>Request Book</Button>          
+          </span>
+        ),
+      }];
+      var returnColumns = [{
+        title: 'Book Name',
+        dataIndex: 'bookName',
+        key: 'bookName',
+      }, {
+        title: 'ISBN',
+        dataIndex: 'isbn',
+        key: 'isbn',
+      },{
+        title: 'Author',
+        dataIndex: 'author',
+        key: 'author',
+      },{
+        title: 'Price($)',
+        dataIndex: 'price',
+        key: 'isbn',
+      },{
+        title: 'Category',
+        dataIndex: 'category',
+        key: 'isbn',
+      },
+      // {
+      //   title: 'Quantity',
+      //   dataIndex: 'availableItems',
+      //   key: 'isbn',
+      // },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <Button 
+            type="primary"
+            onClick={this.returnConfirm.bind(this)}>Return Book</Button>          
+          </span>
+        )
+      }];
+      this.state = {
+        dataSource : defaultDataSource,
+        updatedDataSource:  defaultDataSource,
+        myBooksDataSource:[],// for search purpose
+        updatedMyBooksDataSource:[],//persitence
+        //dataSource:this.masterDataSource,    
+        columns : requestColumns,
+        returnColumns:returnColumns,
+        selectedSearchType:"bookName" ,
+        selectedRowKeys: [], 
+        visible: false     
+      };
+  }
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+    debugger;
+    var updatedMyBooksDataSource = this.state.myBooksDataSource;
+    var updatedBooksDataSource = [];
+    var selectedKey = this.state.selectedRowKeys[0];
+    var allowUpdate = true;
+    this.state.updatedDataSource.forEach(function(dataSource) {  
+          
+        if(dataSource.key.toLowerCase() === selectedKey){
+         
+          if(dataSource.availableItems === 0)
+            allowUpdate = false;
+          dataSource.availableItems = allowUpdate ? dataSource.availableItems - 1 : 0;
+          if(allowUpdate)
+            updatedMyBooksDataSource.push(dataSource);
+          else{
+            alert("Out of stock");
+            return false;
+          }
+        }
+        updatedBooksDataSource.push(dataSource);
+    });
+    if(allowUpdate){
+      this.setState({updatedDataSource:updatedBooksDataSource,myBooksDataSource:updatedMyBooksDataSource,selectedRowKeys:[],updatedMyBooksDataSource:updatedMyBooksDataSource});
+      notification["success"]({
+        message: 'Success',
+        description: 'Book lended successfully.',
+        duration:3
+      });
+  }
+  }
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+  tabCallback(key) {
+    console.log(key);
+  }
+  onSelectChange = (selectedRowKeys) => {
+    debugger;
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    this.setState({ selectedRowKeys : selectedRowKeys});
+  }
+  requestConfirm(){
+    debugger;
+    if(this.state.selectedRowKeys.length > 0){
+    this.setState({
+      visible: true
+    }); 
+  }      
+    else
+      alert("Please select an item!");
+  }
+  returnConfirm(){
+
+  }
+
+     filterDataSource(value,tabName){
+       debugger;
+       var valueLowerCase = value.trim().toLowerCase();
+       var filteredDataSource = [];
+       var selectedSearchType = this.state.selectedSearchType;
+       if(tabName === "requestbooks"){
+       defaultDataSource.forEach(function(dataSource) {
+  
+       if(dataSource[selectedSearchType].toString().toLowerCase().indexOf(valueLowerCase) !== -1)
+          filteredDataSource.push(dataSource);
+       });    
+       this.setState({dataSource:filteredDataSource});
+      }else{
+        this.state.updatedMyBooksDataSource.forEach(function(dataSource) {          
+          if(dataSource[selectedSearchType].toString().toLowerCase().indexOf(valueLowerCase) !== -1)
+            filteredDataSource.push(dataSource);
+          });    
+          this.setState({myBooksDataSource:filteredDataSource});
+      }
+     }
+     handleSearchTypeChange(e){
+       this.setState({selectedSearchType:e});
+     }
+     resetSearch(tabName){
+      if(tabName === "requestbooks")
+        this.setState({dataSource:defaultDataSource});
+      else
+        this.setState({myBooksDataSource:this.state.updatedMyBooksDataSource});
+     }
+     render() {
+        return (          
+            <BookComponent
+                {...{
+                    book: {
+                        dataSource: this.state.dataSource,
+                        myBooksDataSource:this.state.myBooksDataSource,
+                        columns: this.state.columns,
+                        returnColumns: this.state.returnColumns,
+                        selectedRowKeys: this.state
+                    },
+                    model:{
+                      visible: this.state.visible                      
+                    }
+                }}
+                onOk={this.handleOk.bind(this)}
+                onCancel={this.handleCancel.bind(this)}
+                resetSearch={this.resetSearch.bind(this)}
+                handleSearchTypeChange={this.handleSearchTypeChange.bind(this)}
+                filterDataSource={this.filterDataSource.bind(this)}
+                tabCallback={this.tabCallback.bind(this)}
+                onSelectChange={this.onSelectChange.bind(this)}
+            />           
+        );
+    }
+  }
+    export default BookContainer;
